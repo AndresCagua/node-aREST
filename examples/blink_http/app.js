1c8fc97c12b5d5c57b98b8de7994fafedd818160
martinKindall <mart256@gmail.com>
2020-06-20 17:39:30 -0400

    Deleted old example files. Added simple example using express.

---
+++ examples/blink_http/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var rest = require("arest")(app);
+
+rest.addDevice('serial','COM4', 19200);
+
+app.listen(3000, '127.0.0.1', function(){
+    console.log("The frontend server is running on port 3000!")
+});
